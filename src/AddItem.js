fcef8f5f213280a9070597b9a94bf13cd5b2d512
JuSan7 <hanthijusan.wytu2012@gmail.com>
2024-02-08 14:53:54 +0630

    my first commit

---
+++ src/AddItem.js
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+import { FaPlus } from "react-icons/fa";
+const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+  const inputRef = useRef("");
+  return (
+    <form className="addForm" onSubmit={(e) => handleSubmit(e)}>
+      <label htmlFor="addItem"> Add Item</label>
+      <input
+        ref={inputRef}
+        autoFocus
+        id="addItem"
+        placeholder="Add Item"
+        type="text"
+        required
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+      <button
+        type="submit"
+        aria-label="Add Item"
+        onClick={() => inputRef.current.focus()}
+      >
+        <FaPlus />
+      </button>
+    </form>
+  );
+};
+
+export default AddItem;
